07c34a39d9e31fbddaab8247cbcf6b1441b46a9b
Davi <davimatosc@hotmail.com>
2022-04-03 09:39:56 -0300

    creating standard error output

---
+++ back-end/f30/src/main/java/com/carvalho/trinta/f30/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.carvalho.trinta.f30.resources.exceptions;
+
+
+import com.carvalho.trinta.f30.services.exceptions.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+        StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+}
