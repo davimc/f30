eb94b651b9cd59fc6e4624890cc1e5c6c526ce2d
Davi <davimatosc@hotmail.com>
2022-04-07 23:08:33 -0300

    Using MoldelMapper

---
+++ back-end/f30/src/main/java/com/carvalho/trinta/f30/config/MapperConfig.java
@@ -0,0 +1,14 @@
+package com.carvalho.trinta.f30.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
