4f7d528a064ef9a5d24a41e2d977f2381c3c44a9
Davi <davimatosc@hotmail.com>
2022-04-24 09:47:38 -0300

    Standart error message error for Validation exceptions

--- back-end/f30/src/main/java/com/carvalho/trinta/f30/resources/exceptions/ResourceExceptionHandler.java
+++ back-end/f30/src/main/java/com/carvalho/trinta/f30/resources/exceptions/ResourceExceptionHandler.java
@@ -4,10 +4,14 @@ package com.carvalho.trinta.f30.resources.exceptions;
 import com.carvalho.trinta.f30.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
@@ -16,4 +20,17 @@ public class ResourceExceptionHandler {
         StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
     }
+
+    /* TODO verificar como colocar a quebra de linha
+    * está aparecendo \nMensagem de erro
+    */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> methodArgumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request) {
+        String error = "Erro de Validação:";
+        for(ObjectError err : e.getBindingResult().getAllErrors())
+            error = "\n" + err.getDefaultMessage();
+        StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), error, System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+
 }
