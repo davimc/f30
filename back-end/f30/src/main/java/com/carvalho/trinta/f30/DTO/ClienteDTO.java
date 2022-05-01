17a995c492bed856d496468cfd15363e0555d96d
Davi <davimatosc@hotmail.com>
2022-05-01 07:56:47 -0300

    removing validation

--- back-end/f30/src/main/java/com/carvalho/trinta/f30/DTO/ClienteDTO.java
+++ back-end/f30/src/main/java/com/carvalho/trinta/f30/DTO/ClienteDTO.java
@@ -1,12 +1,6 @@
 package com.carvalho.trinta.f30.DTO;
 
 import com.carvalho.trinta.f30.domains.Cliente;
-import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.br.CPF;
-
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.HashSet;
@@ -16,20 +10,10 @@ public class ClienteDTO implements Serializable {
     private static final Long serialVersionUID = 1L;
 
     private Long id;
-    @NotNull(message = "Nome não pode ser nulo")
-    @Length(min= 3, max =80, message="O tamanho do nome deve ser entre 3 a 80 caracteres")
     private String nome;
-    @NotNull(message = "CPF não pode ser nulo")
-    @CPF(message = "formato do CPF está incorreto")
     private String cpf;
-
-    @NotNull(message = "e-mail não pode ser nulo")
-    @Email(message = "É necessário registrar um e-mail válido")
     private String email;
-
     private LocalDate dataNascimento;
-
-    @NotEmpty(message = "Você precisa informar ao menos um telefone para contato")
     private Set<String> telefone = new HashSet<>();
 
     public ClienteDTO() {
